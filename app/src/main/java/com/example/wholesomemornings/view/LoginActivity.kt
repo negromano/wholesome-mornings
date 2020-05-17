578ec0d3969f19492853c3125212926f89e75142
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2020-05-17 15:37:30 -0500

    [#3] Added basic Login Activity view

---
+++ app/src/main/java/com/example/wholesomemornings/view/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.wholesomemornings.view
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.wholesomemornings.R
+
+class LoginActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
