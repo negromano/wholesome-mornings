5226fc64b5f049b6e8959e318b2e000cbe704361
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2020-05-18 19:06:14 -0500

    Closes [#2] Added base home layout

---
+++ app/src/main/java/com/example/wholesomemornings/view/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.wholesomemornings.view
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.wholesomemornings.R
+
+class HomeActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+}
