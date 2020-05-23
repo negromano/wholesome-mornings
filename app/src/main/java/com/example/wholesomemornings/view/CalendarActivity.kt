db6b780934bcac23c1ac430e9f71226eb473eddc
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2020-05-22 19:38:55 -0500

    [#4] Created basic calendar view

---
+++ app/src/main/java/com/example/wholesomemornings/view/CalendarActivity.kt
@@ -0,0 +1,14 @@
+package com.example.wholesomemornings.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wholesomemornings.R
+
+class CalendarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+    }
+
+}
