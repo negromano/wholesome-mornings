4aad8c98814d2682e094e8926fa840180cbd9c54
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2020-07-26 19:00:46 -0500

    [#48][#49] Added Room as database ORM

--- app/src/main/java/com/example/wholesomemornings/model/ListedActivity.kt
+++ app/src/main/java/com/example/wholesomemornings/model/ListedActivity.kt
@@ -1,4 +1,12 @@
 package com.example.wholesomemornings.model
 
-class ListedActivity(type: String, name: String) : Activity(type, name) {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ListedActivity(
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "type") val type: String?,
+    @ColumnInfo(name = "name") val name: String?
+)
